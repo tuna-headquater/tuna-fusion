b44777981a87f1b9b730d69a0ade6f4a53eb8fc2
long.qul <qul2@chinatelecom.cn>
2025-08-12 16:10:09 +0800

    ci: update image build process and permissions
    - Update Docker image building script to use dynamic namespace
    - Add configmaps to RBAC permissions for tuna-fusion-executor
    - Update global image tag in values.yaml
    - Add build-all-images.sh to .dockerignore

--- build-all-images.sh
+++ build-all-images.sh
@@ -4,14 +4,14 @@ set -ex
 image_tag=$(date +%s)
 namespace=ghcr.io/tuna-headquater
 
-echo "Build robinqu/fusion-a2a-fastapi-runtime"
+echo "Build $namespace/fusion-a2a-fastapi-runtime"
 docker buildx build --push --platform linux/amd64,linux/arm64 \
   -t $namespace/fusion-a2a-fastapi-runtime:"$image_tag" \
   -t $namespace/fusion-a2a-fastapi-runtime:latest \
   --label "org.opencontainers.image.source=https://github.com/tuna-headquater/tuna-fusion" \
  ./fusion-a2a-env/fastapi_runtime
 
-echo "Build robinqu/fusion-a2a-env-builder"
+echo "Build $namespace/fusion-a2a-env-builder"
 docker buildx build --push --platform linux/amd64,linux/arm64 \
   -t $namespace/fusion-a2a-env-builder:"$image_tag" \
   -t $namespace/fusion-a2a-env-builder:latest  \
@@ -21,7 +21,7 @@ docker buildx build --push --platform linux/amd64,linux/arm64 \
 modules=("tuna-fusion-operator" "tuna-fusion-gitops-server" "tuna-fusion-executor")
 
 for module in "${modules[@]}"; do
-    echo "Build robinqu/$module"
+    echo "Build $namespace/$module"
     docker buildx build --push \
       --platform linux/amd64,linux/arm64 \
       -t $namespace/"$module":"$image_tag" \
